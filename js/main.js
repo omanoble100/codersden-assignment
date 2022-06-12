496e78ddf018b5923233bbbe39a634db9006cbc1
omanoble100 <amosoma100@gmail.com>
2022-06-12 19:48:02 +0100

    loop qst 13

--- js/main.js
+++ js/main.js
@@ -691,7 +691,28 @@ let totalNum=0
     let sum=0
     for(let i = 0; i < 101; i++){
         sum = i + sum;
-
-        console.log(sum)
     }
 
+    console.log(sum)
+
+    // Qst 12
+    let arp = []
+    let soo=0;
+    let bee=0;
+    for(let i = 0; i<=100; i++){
+        if(i % 2 == 0){
+            soo = soo + i
+            
+        }
+        else{
+            bee = bee + i
+        }
+    }
+    console.log('The sum of all evens from 0 to 100 is ' + soo + ' .' + 'The sum of all odds from 0 to 100 is ' + bee)
+
+    arp.push(soo)
+    arp.push(bee)
+
+    console.log(arp)
+
+   
